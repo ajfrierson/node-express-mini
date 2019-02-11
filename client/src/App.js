2f74f8640ea385e8833dbb10c7efb6c519f6f418
ajfrierson <alvinfrierson@fuller.edu>
2019-02-11 14:43:46 -0800

    I have the BE and React app both working.

---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users:[]
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/users/')
+      .then(response => {
+        this.setState({users: response.data});
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div>
+        {this.state.users.map(user => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <h2>{user.bio}</h2>
+            </div>
+          )
+        })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,1 +1,111 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const cors = require('cors');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const sendUserError = (status, message, res) => {
+  res.status(status).json({
+    errorMessage: message
+  });
+  return;
+}
+
+server.get('/api/users', (req, res) => {
+  db.find().then(response => {
+    res.status(200).json(response)
+  }).catch(err => {
+    res.status(500).json({
+      error: "The users information could not be retrieved."
+    });
+    return;
+  });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id).then(response => {
+    console.log('ss', response.length)
+    if (response.length > 0) {
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      })
+      return;
+    }
+  }).catch(err => {
+    console.log("some Other err:", err);
+  })
+})
+
+server.post('/api/users', (req, res) => {
+  const {
+    name,
+    bio,
+    created_at,
+    updated_at
+  } = req.body;
+  if (!(req.body.name && req.body.bio)) {
+    sendUserError(400, 'Please provide name and bio for the user.', res);
+    return;
+  } else {
+    db.insert(req.body).then(response => {
+      db.findById(response.id).then(response => {
+        res.status(201).json(response);
+      }).catch(err => {
+        sendUserError(500, "User could not be added to the database", res);
+      });
+    }).catch(err => {
+      console.log(err);
+      sendUserError(500, errr, res);
+      return;
+    })
+  }
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  db.remove(req.params.id).then(response => {
+    if (response === 0) {
+      sendUserError(404, "The user with the specified ID does not exist.", res);
+      return;
+    }
+    res.status(200).json(response);
+  }).catch(err => {
+    sendUserError(500, err, res);
+    return;
+  });
+})
+
+server.put('/api/users/:id', (req, res) => {
+  const {
+    name,
+    bio
+  } = req.body;
+  if (!(name && bio)) {
+    sendUserError(400, 'Please provide name and bio for the user.', res);
+    return;
+  }
+
+  db.update(req.params.id, {
+    name,
+    bio
+  }).then(response => {
+    if (response === 0) {
+      sendUserError(404, "The user with the specified ID does not exist.", res);
+      return;
+    }
+    db.findById(req.params.id).then(response => {
+      res.status(200).json(response)
+    }).catch(err => {
+      sendUserError(500, "User could not be updated", res);
+    })
+  }).catch(err => {
+    sendUserError(500, err, res);
+    return;
+  })
+})
+
+server.listen(3000, () => console.log('App is listening...'));
